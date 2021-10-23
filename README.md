219d63b2f50076437704af9ccaf136b6e5ae55e2
Thaer Jomhawi <thaer.fjomhawi@gmail.com>
2021-10-23 19:55:51 +0300

    Lab09

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# quotes
+# Quotes
+Include a method `getQuote()` that Use the file quotes.json to show random popular book quotes. The app showing one quote each time and showing the quote and the author when it is run.
+
+## install dependencies and run the app
+
+### Add the dependency :
+
+add this statement in the build.gradle like this:
+
+```java
+ dependencies {
+  implementation 'com.google.code.gson:gson:2.8.8'
+}
+```
+
+### To Run
+
+1. Press run and the app will show the result of Quote with author Name
+
+### To Test  :
+
+1. with intelliJ by running the test.
+2. by using `./gradlew test`.
+
--- app/src/main/java/quotes/App.java
+++ app/src/main/java/quotes/App.java
@@ -5,7 +5,11 @@ package quotes;
 
 
 
-import java.io.FileReader;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
 import com.google.gson.Gson;
 
@@ -16,8 +20,28 @@ public class App {
 
     public static void main(String[] args) {
 
-        String path ="C:\\Users\\user\\Desktop\\code401\\quotes\\app\\src\\main\\resources\\quotes.json";
-        getQuote(path);
+        if (netIsAvailable()){
+            String url = "http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en";
+            sendGetRequest(url);
+        }else{
+            String path ="C:\\Users\\user\\Desktop\\code401\\quotes\\app\\src\\main\\resources\\quotes.json";
+            getQuote(path);
+        }
+
+    }
+
+    private static boolean netIsAvailable() {
+        try {
+            final URL url = new URL("http://www.google.com");
+            final URLConnection conn = url.openConnection();
+            conn.connect();
+            conn.getInputStream().close();
+            return true;
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            return false;
+        }
     }
 
     public static void getQuote(String path){
@@ -31,9 +55,6 @@ public class App {
             int randomQuote = (int)(Math.random() * quotes.length);
             System.out.println(quotes[randomQuote]);
 
-//            for (int i = 0; i < quotes.length; i++){
-//                System.out.println(quotes[i]);
-//            }
 
             // close reader
             fileReader.close();
@@ -44,6 +65,49 @@ public class App {
 
     }
 
+    static void sendGetRequest(String urlString){
+        try {
+            URL url = new URL(urlString);
+            HttpURLConnection connection = setUpConnectionObject(url);
+            if(connection.getResponseCode() == 200){
+                BufferedReader in = getBufferedReaderFromConnection(connection);
+                printBufferedReaderContect(in);
+                in.close();
+            }
+        } catch (MalformedURLException e) {
+            System.out.println("Sorry, there was a problem creating the URL object,the error was:");
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("Sorry, there was a problem opening the connection from the URL object, the error was:");
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+
+    }
+
+    static HttpURLConnection setUpConnectionObject(URL url) throws IOException {
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
+        return connection;
+    }
+
+    static BufferedReader getBufferedReaderFromConnection(HttpURLConnection connection) throws IOException {
+        InputStream inputStream = connection.getInputStream();
+        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+        BufferedReader in = new BufferedReader(inputStreamReader);
+
+        return in;
+    }
+
+    static void printBufferedReaderContect(BufferedReader in) throws IOException {
+        String line = in.readLine();
+        while(line != null){
+            System.out.println(line);
+            line = in.readLine();
+        }
+    }
 
 
 }
